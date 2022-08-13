2f465943ddac5b79ea92ea9179e76dabfcec4f5f
yoshinashi <ec2-user@ip-172-31-27-130.ap-northeast-1.compute.internal>
2022-08-13 19:45:56 +0900

    ブログの作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(3)]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Post $post, PostRequest $request) 
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/posts', 'PostController@index');
+Route::get('/','PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}','PostController@show');
+Route::post('posts','PostController@store');
