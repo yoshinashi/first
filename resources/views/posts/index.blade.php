56f671b473e5c8e5e5aaaf2e08067a5083de4e93
yoshinashi <ec2-user@ip-172-31-27-130.ap-northeast-1.compute.internal>
2022-08-21 14:21:50 +0900

    ブログ投稿の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,11 +8,17 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        
 
         [<a href='/posts/create'>create</a>]
 
         <div class='posts'>
             @foreach ($posts as $post)
+            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+             @csrf
+            @method('DELETE')
+            <button type="submit">delete</button> 
+            </form>
                 <div class='post'>
                     <h2 class='title'>
                         <a href="/posts/{{ $post->id }}">  {{ $post->title }}</a>
@@ -26,5 +32,16 @@
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        
+       <script>
+        function deletePost(e){
+        'use strict'
+        if (confirm('削除すると復元できません\本当に削除しますか')){
+            document.getElementById('form_delete').submit();
+        }
+        }
+        
+        </script>
+        
     </body>
 </html>
